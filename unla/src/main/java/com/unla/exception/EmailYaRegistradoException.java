1569195caec6e49cdd29dfcf670ae01116b025b0
JulianTomczak <julian0graff0metal@gmail.com>
2025-08-08 15:14:09 -0300

    inicio

--- unla/src/main/java/com/unla/exception/EmailYaRegistradoException.java
+++
@@ -1,8 +0,0 @@
-package com.unla.exception;
-
-public class EmailYaRegistradoException extends RuntimeException {
-    public EmailYaRegistradoException(String email) {
-        super("Ya existe un usuario registrado con el email: " + email);
-    }
-
-}
